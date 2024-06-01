ca8589a1e05a95ce3b45e59bc6feafd8e416f47e
Alex Bacila <bacila.a2002@yahoo.com>
2024-06-01 21:46:16 +0300

    Add project files.

---
+++ Asamblor/Program.cs
@@ -0,0 +1,23 @@
+﻿string filePath = "path/to/your/file.asm";
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("File does not exist.");
+}
+
+try
+{
+    using (StreamReader sr = new StreamReader(filePath))
+    {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
